3768477a6230fcb5910cf50e090edb59e9751020
Lawrence Win <winlarry@microsoft.com>
2023-09-22 15:41:23 -0400

    Fix rebase conflicts

--- packages/experimental/TabList/src/TabListAnimatedIndicator/TabListAnimatedIndicator.tsx
+++ packages/experimental/TabList/src/TabListAnimatedIndicator/TabListAnimatedIndicator.tsx
@@ -2,19 +2,23 @@
 import React from 'react';
 import { Animated } from 'react-native';
 
-import { mergeProps, stagedComponent } from '@fluentui-react-native/framework';
+import { stagedComponent } from '@fluentui-react-native/framework';
 
-import type { TabListAnimatedIndicatorProps } from './TabListAnimatedIndicator.types';
 import { tablistAnimatedIndicatorName } from './TabListAnimatedIndicator.types';
+import { useAnimatedIndicatorStyles } from './useAnimatedIndicatorStyles';
 
-export const TabListAnimatedIndicator = stagedComponent(() => (props: TabListAnimatedIndicatorProps) => {
-  const { styles, ...rest } = props;
-  const rootProps = mergeProps(rest, { style: styles.container });
-  return (
-    <Animated.View {...rootProps}>
-      <Animated.View style={styles.indicator} />
-    </Animated.View>
-  );
+export const TabListAnimatedIndicator = stagedComponent(() => {
+  const styles = useAnimatedIndicatorStyles();
+  return () => {
+    if (!styles) {
+      return null;
+    }
+    return (
+      <Animated.View style={styles.container}>
+        <Animated.View style={styles.indicator} />
+      </Animated.View>
+    );
+  };
 });
 TabListAnimatedIndicator.displayName = tablistAnimatedIndicatorName;
 
